add6b5ab94402c9912ebafaf66e569e70a9867b2
pro100n00b <127980856+pro100n00b@users.noreply.github.com>
2023-09-30 15:06:18 +0400

    Update calc.java

--- calc.java
+++ calc.java
@@ -1,168 +1,118 @@
+import java.util.List;
 import java.util.Scanner;
-import java.util.Arrays;
 
 class calc {
+    static List<String> romanDigit = List.of("0", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX", "X");
+    static List<String> romanTens = List.of("0", "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC", "C");
 
     public static void main(String[] args) {
-        while (true) {
+        calc(String.valueOf(0));
+    }
+    public static String calc(String input){
+            while (true) {
             System.out.println("Введите значения (a + b,a / b, a * b, a - b):");
             Scanner scanner = new Scanner(System.in);
             String s = scanner.nextLine();
             String[] nums = s.split(" ");
 
-
-            int check = nums.length;
-            if (check > 3){
-                throw new RuntimeException();
+            if (nums.length != 3) {
+                throw new RuntimeException("Не верный синтаксис");
             }
-            String[] stroka9 = {"0", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX", "X"};
-            String[] stroka10 = {"0", "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC", "C"};
-            try {
-                String firstnum = nums[0];
-                String operation = nums[1];
-                String secondnum = nums[2];
-            }  catch (ArrayIndexOutOfBoundsException e) {
-                throw new RuntimeException();
-
-            }  catch (StringIndexOutOfBoundsException e) {
-                throw new RuntimeException();
-        }
-            String firstnum = nums[0];
+            String firstDigit = nums[0];
             String operation = nums[1];
-            String secondnum = nums[2];
-            try {
+            String secondDigit = nums[2];
+            
+            boolean asd = (Character.isDigit(firstDigit.charAt(0)));
+            boolean dsa = (Character.isDigit(secondDigit.charAt(0)));
+            String result;
+            
+            if (asd && dsa) {
+
+                result = arabicOperation(firstDigit, secondDigit, operation);
+
+            } else if (!asd && !dsa) {
+
+                result = romanOperation(firstDigit, secondDigit, operation);
 
 
-                boolean asd = (Character.isDigit(firstnum.charAt(0)));
-                boolean dsa = (Character.isDigit(secondnum.charAt(0)));
-            }catch (StringIndexOutOfBoundsException e){
-                throw new RuntimeException();
-            }
-
-
-
-            boolean asd = (Character.isDigit(firstnum.charAt(0)));
-            boolean dsa = (Character.isDigit(secondnum.charAt(0)));
-            if (asd == dsa) {
-                if (asd == true) {
-                    int num_one = Integer.parseInt(firstnum);
-                    int num_two = Integer.parseInt(secondnum);
-                    if( num_one  > 10)  {
-                        throw new RuntimeException();
-
-                    }
-                    if( num_two  > 10)  {
-                        throw new RuntimeException();
-                    }
-                    switch (operation) {
-                        case "-":
-                            int result = num_one - num_two;
-                            System.out.println(result);
-                    }
-                    switch (operation) {
-                        case "+":
-                            int result = num_one + num_two;
-                            System.out.println(result);
-                    }
-
-                    switch (operation) {
-                        case "/":
-                            int result = num_one / num_two;
-                            System.out.println(result);
-                    }
-
-                    switch (operation) {
-                        case "*":
-                            int result = num_one * num_two;
-                            System.out.println(result);
-                    }
-
-                } else if (asd == false) {
-
-                    int index_one = Arrays.asList(stroka9).indexOf(firstnum);
-                    int index_two = Arrays.asList(stroka9).indexOf(secondnum);
-
-                    if (index_two == -1){
-                        throw new RuntimeException();
-                    }
-                    if (index_one == -1){
-                        throw new RuntimeException();
-                    }
-
-                    int result = 0;
-                    switch (operation) {
-                        case "-":
-                            result = index_one - index_two;
-                            break;
-                    }
-                    switch (operation) {
-                        case "+":
-                            result = index_one + index_two;
-                            break;
-                    }
-
-                    switch (operation) {
-                        case "/":
-                            result = index_one / index_two;
-                            break;
-                    }
-
-                    switch (operation) {
-                        case "*":
-                            result = index_one * index_two;
-                            break;
-                    }
-                    if (result <= 0){
-                        throw new RuntimeException();
-                    }
-                    String last_result = Integer.toString(result);
-                    int tenorone = last_result.length();
-
-
-                    if (tenorone != 1) {
-                        char[] charArray = last_result.toCharArray();
-
-                        int[] intArray = new int[charArray.length];
-                        for (int i = 0; i < charArray.length; i++) {
-                            intArray[i] = Character.getNumericValue(charArray[i]);
-                        }
-
-                        String number = Arrays.asList(stroka10).get(intArray[0]);
-                        String anothernum = Arrays.asList(stroka9).get(intArray[1]);
-                        switch (number) {
-                            case "0":
-                                number = "";
-                                break;
-                        };
-
-                        switch (anothernum) {
-                            case "0":
-                                anothernum = "";
-                                break;
-                        };
-
-                        String out_num = number + anothernum;
-
-                        switch (result) {
-                            case 100:
-                                out_num = "C";
-                                break;
-                        }
-
-                        System.out.println(out_num);
-
-                    } else {
-                        int daj = Integer.parseInt(last_result);
-                        String number = Arrays.asList(stroka9).get(daj);
-                        System.out.println(number);
-
-                    }
-                }
             } else {
-                throw new RuntimeException();
-
+                throw new RuntimeException("Нельзя использовать два разных вида цисел");
             }
+            System.out.println(result);
+        }
+    }
+    
+    private static String romanOperation(String firstDigit, String secondDigit, String operation) {
+        int result;
+        int firstIndexDigit = romanDigit.indexOf(firstDigit);
+        int secondIndexDigit = romanDigit.indexOf(secondDigit);
+
+        if (secondIndexDigit == -1) {
+            throw new RuntimeException("Неверное значение");
+        }
+        if (firstIndexDigit == -1) {
+            throw new RuntimeException("Неверное значение");
+        }
+
+        switch (operation) {
+            case "-" -> result = firstIndexDigit - secondIndexDigit;
+            case "+" -> result = firstIndexDigit + secondIndexDigit;
+            case "/" -> result = firstIndexDigit / secondIndexDigit;
+            case "*" -> result = firstIndexDigit * secondIndexDigit;
+            default -> throw new RuntimeException("Неверная операция");
+        }
+        if (result <= 0) {
+            throw new RuntimeException("Результат не может быть меньше/равен 0");
+        }
+        if (result == 100){
+            return "C";
+        }
+        String resultString = Integer.toString(result);
+
+        return getRomanFromArabic(resultString);
+    }
+    
+    private static String getRomanFromArabic(String resultString) {
+
+        if ( resultString.length() != 1){
+        String firstRomanFromArabic = romanTens.get(Integer.parseInt(resultString.substring(0,1)));
+        String secondRomanFromArabc = romanDigit.get(Integer.parseInt(resultString.substring(1,2)));
+        switch (firstRomanFromArabic) {
+            case "0":
+                firstRomanFromArabic = "";
+                break;
+        }
+
+        switch (secondRomanFromArabc) {
+            case "0":
+                secondRomanFromArabc = "";
+                break;
+        }
+
+        return firstRomanFromArabic + secondRomanFromArabc;}
+        else {
+            return romanDigit.get(Integer.parseInt(resultString.substring(0,1)));
+        }
 
     }
-}
-}
+    private static String arabicOperation (String firstDigit, String secondDigit, String operation){
+            int result;
+            int digitOne = Integer.parseInt(firstDigit);
+            int digitTwo = Integer.parseInt(secondDigit);
+            if (digitOne > 10) {
+                throw new RuntimeException("Число больше десяти");
+            }
+        if (digitTwo > 10) {
+            throw new RuntimeException("Число больше десяти");
+
+        }
+            switch (operation) {
+                case "-" -> result = digitOne - digitTwo;
+                case "+" -> result = digitOne + digitTwo;
+                case "/" -> result = digitOne / digitTwo;
+                case "*" -> result = digitOne * digitTwo;
+                default -> throw new RuntimeException("Неверная операция");
+            }
+            return String.valueOf(result);
+        }
+    }
